199860bb5be2d1d909edef144de731605a5e3e13
robynZhao <zhaorobyn895@gmail.com>
2023-08-06 17:26:15 -0400

    simple redux pratice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch} from 'react-redux';
+import { increment, decrement, checkLog } from './actions';
 
 function App() {
+    // access the reducer
+    const counter = useSelector(state => state.counter);
+    const isLogged = useSelector(state => state.isLogged)
+
+    // get ability to dispatch an action
+    const dispatch = useDispatch();
+
   return (
     <div className="App">
-        <h1>Hello</h1>
+        <h1>Counter#: {counter} </h1>
+        <button onClick={() => dispatch(increment(1))}> + 1 </button>
+        {" "}
+        <button onClick={() => dispatch(increment(100))}>  + 100</button>
+        {" "}
+        <button onClick={() => dispatch(decrement(1))}> - 1 </button>
+        {" "}
+        <button onClick={() => dispatch(decrement(100))}> - 100 </button>
+
+        {isLogged ? <h3>Yes! Logged! </h3> : <h3>No! Didn't Log! </h3>}
+        <button onClick={() => dispatch(checkLog())}>Click here to update sign-in or sign-out!</button>
+        {" "}
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+// import all reducers here
+import counterReducer from './counter';
+import loggedReducer from './isLogged';
+import { combineReducers } from 'redux';
+
+const allReducers = combineReducers({
+    counter: counterReducer,
+    isLogged: loggedReducer
+})
+
+export default allReducers;
